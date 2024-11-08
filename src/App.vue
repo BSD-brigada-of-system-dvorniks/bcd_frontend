9d3d2ed87dcbfd8dc29f1937c19203ca18706b70
Makuta <nikita.o.platonov@gmail.com>
2024-11-08 15:59:49 +0200

    Changed page to test one, added .zed to gitgnore

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,24 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+import TestMain from './components/TestMain.vue';
 </script>
 
 <template>
-  <header>
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
     </div>
-  </header>
+  </header> -->
 
-  <main>
-    <TheWelcome />
-  </main>
+  <!-- <main> -->
+    <TestMain />
+  <!-- </main> -->
 </template>
 
-<style scoped>
+<!-- <style scoped>
 header {
   line-height: 1.5;
 }
@@ -44,4 +45,4 @@ header {
     flex-wrap: wrap;
   }
 }
-</style>
+</style> -->
---
+++ src/components/TestMain.vue
@@ -0,0 +1,225 @@
+<script lang="ts">
+export default {
+  mounted() {
+    const bootstrap_link = document.createElement("link");
+    bootstrap_link.rel = "stylesheet";
+    bootstrap_link.href =
+      "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css";
+    bootstrap_link.integrity =
+      "sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH";
+    bootstrap_link.crossOrigin = "anonymous";
+    document.head.appendChild(bootstrap_link);
+
+    const bootstrap_icons = document.createElement("link");
+    bootstrap_icons.rel = "stylesheet";
+    bootstrap_icons.href =
+      "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css";
+    document.head.appendChild(bootstrap_icons);
+
+    const bootstrap_script = document.createElement("script");
+    bootstrap_script.src =
+      "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js";
+    bootstrap_script.integrity =
+      "sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz";
+    bootstrap_script.crossOrigin = "anonymous";
+    document.body.appendChild(bootstrap_script);
+  },
+};
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#"><i class="bi bi-globe"></i> COVENANT</a>
+    </div>
+  </nav>
+
+  <div class="container-fluid p-3">
+    <div class="row justify-content-center">
+      <div class="col-6">
+        <div class="card bg-dark mb-3">
+          <div class="card-body d-flex">
+            <div class="card-body-left">
+              <h3>ASO-1 - Квантовый мяч</h3>
+              <h4>
+                <!--<i class="bi bi-alarm-fill"></i>
+									<i class="bi bi-battery-full"></i>
+									<i class="bi bi-cloud-fill"></i>-->
+                <!--<i class="bi bi-exclamation-diamond-fill"></i>
+									<i class="bi bi-exclamation-diamond-fill"></i>
+									<i class="bi bi-exclamation-diamond-fill"></i>-->
+                <i class="bi bi-diamond-half"></i>
+                <i class="bi bi-x-diamond"></i>
+                <i class="bi bi-exclamation-diamond"></i>
+              </h4>
+              <!--Уровень допуска: 5
+								<br>-->
+              Отдел прикладкой физики
+              <br />
+              Куратор - доктор Хейнцман
+            </div>
+
+            <div
+              class="square d-flex align-items-center justify-content-center"
+            >
+              5
+            </div>
+          </div>
+        </div>
+
+        <div class="card bg-dark">
+          <div class="card-body aso-description">
+            <figure class="figure">
+              <img
+                src="../../old_pages/pictures/aso-1_profile.png"
+                class="figure-img img-fluid rounded"
+                alt="A generic square placeholder image with rounded corners in a figure."
+              />
+              <figcaption class="figure-caption text-center text-light">
+                ASO-1 в камере содержания
+              </figcaption>
+            </figure>
+            <p>
+              Основной текст с описанием аномалии. Основной текст с описанием
+              аномалии. Основной текст с описанием аномалии. Lorem ipsum dolor
+              sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Aliquam vestibulum
+              morbi blandit cursus risus. Aliquam sem fringilla ut morbi
+              tincidunt augue interdum velit. Varius vel pharetra vel turpis
+              nunc eget lorem dolor sed. Odio pellentesque diam volutpat commodo
+              sed egestas. Porttitor rhoncus dolor purus non. In fermentum
+              posuere urna nec. Volutpat odio facilisis mauris sit amet massa
+              vitae tortor. Dui nunc mattis enim ut tellus. Molestie nunc non
+              blandit massa.
+            </p>
+            <p>
+              Imperdiet nulla malesuada pellentesque elit eget gravida cum. Id
+              porta nibh venenatis cras sed. Enim nunc faucibus a pellentesque
+              sit amet porttitor eget. Mauris ultrices eros in cursus turpis
+              massa tincidunt dui ut. Hendrerit gravida rutrum quisque non
+              tellus orci ac. Fames ac turpis egestas sed tempus urna. Pretium
+              lectus quam id leo in vitae turpis. Faucibus et molestie ac
+              feugiat. Sed turpis tincidunt id aliquet risus feugiat in ante
+              metus. Dictum non consectetur a erat nam.
+            </p>
+            <p>
+              Velit euismod in pellentesque massa placerat duis ultricies. Neque
+              volutpat ac tincidunt vitae semper quis lectus nulla. Proin sed
+              libero enim sed faucibus turpis. Aliquet eget sit amet tellus.
+              Ridiculus mus mauris vitae ultricies leo integer malesuada.
+              Habitasse platea dictumst quisque sagittis purus sit. Scelerisque
+              eu ultrices vitae auctor. Mattis enim ut tellus elementum sagittis
+              vitae et leo. Lacus sed turpis tincidunt id aliquet risus feugiat
+              in ante. In fermentum et sollicitudin ac orci. Est ultricies
+              integer quis auctor elit sed vulputate mi sit. Blandit libero
+              volutpat sed cras ornare arcu dui vivamus arcu. Porta non pulvinar
+              neque laoreet suspendisse interdum consectetur libero id. Purus in
+              massa tempor nec feugiat nisl pretium fusce. Aliquam id diam
+              maecenas ultricies. Est pellentesque elit ullamcorper dignissim
+              cras.
+            </p>
+            <div class="quote">
+              <p>
+                Цитата или курсив. Цитата или курсив. Цитата или курсив. Цитата
+                или курсив. Цитата или курсив. Цитата или курсив.
+              </p>
+              <p class="quote-sign text-end">Доктор Хайнцман</p>
+            </div>
+            <p>
+              Основной текст с описанием аномалии. Основной текст с описанием
+              аномалии. Основной текст с описанием аномалии. Egestas fringilla
+              phasellus faucibus scelerisque eleifend donec pretium. Nullam non
+              nisi est sit amet. Amet venenatis urna cursus eget nunc
+              scelerisque viverra mauris in. Quam quisque id diam vel quam
+              elementum pulvinar. Lorem ipsum dolor sit amet consectetur
+              adipiscing elit. Risus viverra adipiscing at in tellus integer
+              feugiat. Consectetur libero id faucibus nisl tincidunt eget nullam
+              non. Fusce ut placerat orci nulla pellentesque dignissim enim sit
+              amet. Proin sagittis nisl rhoncus mattis. Mi sit amet mauris
+              commodo quis imperdiet massa tincidunt nunc. Pellentesque
+              adipiscing commodo elit at imperdiet dui. Neque volutpat ac
+              tincidunt vitae semper quis lectus nulla at. Vestibulum mattis
+              ullamcorper velit sed ullamcorper. Magna sit amet purus gravida.
+            </p>
+            <p>
+              Convallis a cras semper auctor neque. Metus dictum at tempor
+              commodo ullamcorper. Velit laoreet id donec ultrices tincidunt
+              arcu non sodales neque. Egestas purus viverra accumsan in nisl
+              nisi scelerisque eu ultrices. Erat imperdiet sed euismod nisi
+              porta lorem mollis aliquam. Aliquet eget sit amet tellus cras.
+              Turpis egestas maecenas pharetra convallis posuere morbi leo urna.
+              Neque convallis a cras semper auctor. Maecenas accumsan lacus vel
+              facilisis. Justo eget magna fermentum iaculis eu non diam
+              phasellus. Lorem dolor sed viverra ipsum nunc aliquet. Fusce ut
+              placerat orci nulla pellentesque dignissim enim sit amet. Enim
+              nulla aliquet porttitor lacus luctus accumsan tortor posuere ac.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+/*.container {
+				max-width: 600px;
+				margin: auto;
+			}*/
+/*.container-fluid {
+				display: flex;
+				justify-content: center;
+			}*/
+.aso-description {
+  text-align: justify;
+}
+
+.quote {
+  border-left: 5px solid var(--bs-primary);
+  /*#17a2b8;*/
+  padding-left: 15px;
+  margin: 20px 0;
+  font-style: italic;
+  text-align: justify;
+}
+
+.quote-sign {
+  tetx-align: left;
+}
+
+.card {
+  color: var(--bs-light);
+  /*margin-bottom: 1rem;*/
+}
+
+.card-header {
+  /*display: table-row;*/
+  /*align-items: center;*/
+}
+
+.card-header div {
+  /*flex: 1;
+				text-align: center;*/
+  /*font-size: 2rem;
+				font-weight: bold;*/
+  /*background-color: #17a2b8;
+				color: #121212;*/
+}
+
+.square {
+  width: 128px;
+  height: 128px;
+  background-color: var(--bs-warning);
+  /*margin-right: 1rem;
+				display: flex;*/
+  justify-content: center;
+  align-items: center;
+  display: block;
+  font-size: 64px;
+  font-weight: bold;
+}
+
+.card-body-left {
+  width: calc(100% - 128px);
+}
+</style>
