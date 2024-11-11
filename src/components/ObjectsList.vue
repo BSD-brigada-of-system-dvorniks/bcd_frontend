bc23eaffe25515052f57a7e42f502e5201b85663
Taldariner <71024372+Taldariner@users.noreply.github.com>
2024-11-11 18:19:26 +0200

    Tweak to make requests work with news backend API
    Tweaked it a little bit, to make requests (Objects list) work with news backend API.

--- src/components/ObjectsList.vue
+++ src/components/ObjectsList.vue
@@ -13,7 +13,7 @@
 
         methods: {
             async getAnswer() {
-                const { data } = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/objects/`);
+                const { data } = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/articles/objects/`);
                 console.log(data);
                 this.info = data;
                 this.loading = false;
@@ -30,7 +30,7 @@
     <div class="w-screen grid grid-cols-12 gap-4">
         <div></div>
         <div></div>
-        <div class="col-span-4">
+        <div class="col-span-2">
             <div class="bg-zinc-900 text-white border border-white border-dashed p-4 mb-4">
                 <p class="text-xl">BCD</p>
                 <hr class="my-4"/>
@@ -59,24 +59,27 @@
                 </ul>
             </div>
         </div>
-        <div class="col-span-4">
-            <div v-if="!loading" class="bg-zinc-900 text-white border border-white border-dashed p-8">
+        <div class="col-span-6">
+            <div v-if="!loading" class="bg-zinc-900 text-white border border-white border-dashed p-4">
                 <p class="mb-4 text-2xl flex justify-between items-center">
                     <span class="font-logo"><i class="bi bi-globe"></i> COVENANT</span>
                     <span>DOC-01AF</span>
                 </p>
                 <hr class="mb-4">
 
-                <div v-for="n in 4">
+                <div v-for="n in 3">
                     <div v-for="object in info" class="mb-2 hover:ms-4 duration-200">
-                        <p v-if="object.type === 'Green'">
-                            <i class="bi bi-3-square-fill text-green-500"></i> BCD-00AC - {{ object.name }} - {{ object.type }}
+                        <p v-if="object.type === 'Green'" class="flex justify-between cursor-pointer">
+                            <span><i class="bi bi-3-square-fill text-green-500"></i> BCD-00AC - {{ object.article.name }} - {{ object.type }}</span>
+                            <span>Published: {{ object.article.published }}</span>
                         </p>
-                        <p v-if="object.type === 'Yellow'">
-                            [3] BCD-00AC - {{ object.name }} - {{ object.type }}
+                        <p v-if="object.type === 'Yellow'" class="flex justify-between cursor-pointer">
+                            <span>[3] BCD-00AC - {{ object.article.name }} - {{ object.type }}</span>
+                            <span>Published: {{ object.article.published }}</span>
                         </p>
-                        <p v-if="object.type === 'Red'" class="cursor-pointer">
-                            <span class="text-red-500">[3]</span> <span class="underline">BCD-00AC - {{ object.name }}</span>
+                        <p v-if="object.type === 'Red'" class="flex justify-between cursor-pointer">
+                            <span><span class="text-red-500">[3]</span> <span class="underline">BCD-00AC - {{ object.article.name }}</span></span>
+                            <span>Published: {{ object.article.published }}</span>
                         </p>
                     </div>
                 </div>
