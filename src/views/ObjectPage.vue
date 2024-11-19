591f13118a348f3217bba5232b5b54892c3925d3
Makuta <nikita.o.platonov@gmail.com>
2024-11-19 14:14:26 +0200

    Fixed error from typescrpt

--- src/views/ObjectPage.vue
+++ src/views/ObjectPage.vue
@@ -65,19 +65,19 @@ export default {
                         BCD-00{{ object.id.substring(10, 12).toUpperCase() }}
                     </span>
                 </p>
-                
+
                 <hr class="my-4" />
-                
+
                 <p>
                     {{ object.article.text }}
                 </p>
-                
-                <p v-for="n in 5" class="my-4">
+
+                <p v-for="_ in 5" :key="_" class="my-4">
                     But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?
                 </p>
-                
+
                 <hr class="my-4" />
-                
+
                 <p class="place-self-end text-end">
                     <span>{{ object.article.author.username }}</span>
                     <br>
@@ -88,7 +88,7 @@ export default {
         <div></div>
         <div></div>
     </div>
-    
+
 </template>
 
 <style scoped>
