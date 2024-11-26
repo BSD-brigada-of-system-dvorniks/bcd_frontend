939a01d67d5af2901af34356ff4c078c2e828eac
Taldariner <71024372+Taldariner@users.noreply.github.com>
2024-11-26 18:07:10 +0200

    Updated MainComputer component styles
    Updated MainComputer component styles, tried to style it as retro terminal (but partly failed, bruh).

--- src/components/MainComputer.vue
+++ src/components/MainComputer.vue
@@ -36,15 +36,67 @@ export default {
     </div> -->
 
     <!-- Login -->
-    <div class="bg-zinc-900 text-white border border-white border-dashed p-4">
-        <input v-model="loginRequest.username" placeholder="User Name" class="text-black" value="testuser3">
-        <br>
-        <br>
-        <input v-model="loginRequest.password" placeholder="Password" class="text-black" value="Durka1337!">
-        <br>
-        <br>
-        <button @click="submitLogin">Submit</button>
+    <div class="bg-zinc-200 shadow-inner shadow-zinc-700 p-4">
+        <div class="bg-zinc-900 p-0">
+            <div class="select-none py-6 px-4 font-logo text-2xl content-center text-start terminal">
+                <p class="text-terminal mb-6">
+                    You are not authorized. Please, authorize in system before start working.
+                </p>
+                <div class="mb-0 text-terminal">
+                    Username:
+                    <input v-model="loginRequest.username" 
+                        placeholder="User Name" 
+                        class="bg-zinc-900 text-terminal border-0 border-terminal animate-pulse" 
+                        value="testuser3">
+                </div>
+                <div class="mb-6 text-terminal">
+                    Password:
+                    <input v-model="loginRequest.password" 
+                        placeholder="Password" 
+                        class="bg-zinc-900 text-terminal border-0 border-terminal animate-pulse" 
+                        value="Durka1337!">
+                </div>
+                <button class="text-terminal" @click="submitLogin">
+                    > Submit
+                </button>
+            </div>
+        </div>
     </div>
 </template>
 
-<style scoped></style>
+
+<style scoped>
+    *:focus {
+        outline: none;
+    }
+    input {
+        background: none;
+    }
+    .font-logo {
+        font-family: 'Share Tech Mono', monospace;
+    }
+    .terminal {
+        display: block;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        background: linear-gradient(
+            to bottom,
+            rgba(18, 16, 16, 0) 50%,
+            rgba(0, 0, 0, 0.25) 50%
+        );
+        background-size: 100% 4px;
+        z-index: 2;
+    }
+    .text-terminal {
+        color: #13A10E;
+        text-shadow: 0 0 2px #13A10E;
+        font-family: "Share Tech Mono", system-ui;
+        line-height: 1.5rem;
+
+    }
+    .border-terminal {
+        border-color: #13A10E;
+    }
+</style>
